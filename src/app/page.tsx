11b9d74deccac02bcf36da46dc99e559b6a137fa
dhirendravsingh <dhirendravsingh09@gmail.com>
2025-02-08 12:52:46 +0530

    making it production ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default async function Home() {
       <div className="flex flex-col gap-5 items-center justify-center mt-10 w-screen">
         {tasks.map((task)=>(
           <div key={task.id} className="w-full">
-          <Task task={task}/>
+          <Task task={task} key={task.id}/>
           </div>
         ))}
       </div>
